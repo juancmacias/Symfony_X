33b3604998ce2e971128d45429fca2f9946a7066
juan carlos macias <juancmaciassalvador@gmail.com>
2023-06-12 06:12:35 +0200

    Con modificaciones

--- src/Form/AlumnoType.php
+++ src/Form/AlumnoType.php
@@ -3,17 +3,32 @@
 namespace App\Form;
 
 use App\Entity\Alumno;
+//use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AlumnoType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nombre')
-            ->add('apellido')
+            ->add('nombre', TextType::class, array(
+                'label' => "Nombre",
+                'required' => true,
+                'attr' => array('class'=>'form-control',
+                                'placeholder' => 'Pon aquí el nombre del alumno')
+       ))
+
+            ->add('apellido', TextType::class, array(
+                'label' => "Apellido",
+                'required' => true,
+                'attr' => array('class'=>'form-control',
+                                'placeholder' => 'Pon aquí el apellido del alumno')
+        ))
+
         ;
     }
 
